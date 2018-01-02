4499986814b1a945009a34078b216e6b70f7486b
root <root@group2.pilarkto.net>
2018-01-02 15:20:52 +0000

    Installing Tools after Base-install

---
+++ INSTALL_START/install_post_tools.sh
@@ -0,0 +1,20 @@
+echo "Installing Tools..........."
+echo "1. Screen"
+emerge app-misc/screen
+echo "2. git & eix"
+emerge -D app-portage/eix dev-vcs/git
+eix-update
+echo "3. nullmailer, gentoolkit, cron"
+emerge -D app-portage/gentoolkit mail-mta/nullmailer virtual/cron
+emerge --config nullmailer
+rc-update add nullmailer
+CRONNAME="${ls /etc/init.d/ |grep cron}"
+rc-update add $CRONNAME
+echo "4. getting pnet-suite"
+cd /usr/share
+git clone https://github.com/pvsa/pnet-suite.git
+echo "Finish"
+echo "following atoms are in world:"
+cat /var/lib/portage/world
+
+
